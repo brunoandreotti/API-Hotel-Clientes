8fae4affbff05a66d1327e6373b8e90b4a876be2
Bruno Andreotti <brunoandreotti@outlook.com>
2022-03-10 16:23:10 -0300

    Estrutura base e conexão com o banco de dados

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv-safe/config.js')
+const express = require('express')
+
+//Conexão com o banco
+const sequelize = require('./src/database/connection.js')
+
+const app = express()
+
+//Ler e enviar JSON na req e res
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+
+
+//Sincronização com o banco
+async function sync() {
+  try {
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+  } catch (error) {
+    console.log(`Houve um erro ao sincronizar com o banco: ${error}`)
+  }
+}
+
+sync()
