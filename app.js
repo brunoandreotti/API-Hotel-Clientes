751316f94e607b50abc0e4e31d7df717721d9122
Bruno Andreotti <77709788+brunoandreotti@users.noreply.github.com>
2022-03-15 15:51:21 -0300

    Revert "Feature/delete cliente"

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 require('dotenv-safe/config.js')
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT
 
 //Conexão com o banco
 const sequelize = require('./src/database/connection.js')
 
-//Models
-const Cliente = require('./src/models/clienteModel.js')
-
-//Routers
-const clienteRoutes = require('./src/routes/clienteRoutes.js')
+const app = express()
 
 //Ler e enviar JSON na req e res
 app.use(express.json())
 
-//Rotas
-app.use('/clientes', clienteRoutes)
+const PORT = process.env.PORT
+
 
 
 //Sincronização com o banco
